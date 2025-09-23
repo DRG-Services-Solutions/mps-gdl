46d66868edd12b8f084538416a6a626ca6bc6852
Erick Betancourt Garcia <erickbeta12@gmail.com>
2025-09-23 09:42:38 -0600

    Agregado colores a los componentes de acorde a marca y logotipo

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -9,9 +9,9 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <a href="{{ route('users.create') }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 mb-4">
+                    <x-primary-button>
                         Crear Nuevo Usuario
-                    </a>
+                    </x-primary-button>
                     
                     <div class="overflow-x-auto">
                         <table class="min-w-full divide-y divide-gray-200">
