b4f038a37674b1d1da7d61e80d8d3567ae192ed7
Erick Betancourt <ebetancourt@drg.mx>
2026-01-03 08:38:30 -0600

    se elimina seeder de hospital generico, se modifica seeder de hospital general para agregar mas hospitales

--- database/seeders/HospitalSystemSeeder.php
+++ database/seeders/HospitalSystemSeeder.php
@@ -17,23 +17,42 @@ class HospitalSystemSeeder extends Seeder
         $seguro = Modality::create(['name' => 'Seguro']);
         $particular = Modality::create(['name' => 'Particular']);
 
-        //  Hospital
-        $hospital = Hospital::create([
-            'name' => 'Hospital General San José',
-            'rfc' => 'HGS900909H12',
+        //hospital 1 de prueba
+        $hospital1 = Hospital::create([
+            'name' => 'CENTRO HOSPITALARIO MAC  S.A. DE C.V.',
+            'rfc' => 'CHO0801174Z5',
             'is_active' => true
         ]);
+
+        //  Hospitales
+        $hospital =([
+            [
+                'name' => 'AMERICAS HOSPITAL SA DE CV',
+                'rfc' => 'AHO960911E71',
+                'is_active' => true
+            ],
+        
+            [
+                'name' => 'Hospital General San José',
+                'rfc' => 'HGS900909H12',
+                'is_active' => true
+            ],
+        ]);
+
+        foreach ($hospital as $hospitalData) {
+            Hospital::create($hospitalData);
+        }
 
         // 4. Crear la Configuración Híbrida 
         // Ejemplo: Este hospital usa la Entidad 1 para Seguros y la Entidad 2 para Particulares
         HospitalModalityConfig::create([
-            'hospital_id' => $hospital->id,
+            'hospital_id' => $hospital1->id,
             'modality_id' => $seguro->id,
             'legal_entity_id' => "1",
         ]);
 
         HospitalModalityConfig::create([
-            'hospital_id' => $hospital->id,
+            'hospital_id' => $hospital1->id,
             'modality_id' => $particular->id,
             'legal_entity_id' => "2"
             
